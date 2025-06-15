d3e7327990bc88032e813596ad7abbf131bc75f4
Madhankumar425 <madhanvenkat3457@gmail.com>
2025-06-15 13:04:17 +0530

    Time: 5 ms (85.50%) | Memory: 56.1 MB (92.83%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,28 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums){
-            set.add(i);
+        // Set<Integer> set = new HashSet<>();
+        // for(int i : nums){
+        //     set.add(i);
+        // }
+        // int freq[] = new int[100001];
+        // for(int i : set){
+        //     freq[i]++;
+        // }
+        // //
+        // for(int j=1;j<nums.length+1;j++){
+        //     if(freq[j]==0) list.add(j);
+        // }
+        // return list;
+       int freq[] = new int[100001];
+       for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+       }
+       for(int i=1;i<nums.length+1;i++){
+        if(freq[i]==0){
+            list.add(i);
         }
-        int freq[] = new int[100001];
-        for(int i : set){
-            freq[i]++;
-        }
-        //
-        for(int j=1;j<nums.length+1;j++){
-            if(freq[j]==0) list.add(j);
-        }
+       }
         return list;
     }
 }
